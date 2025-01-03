38dc1b5a8681c637127670faec043c47729fb6ac
Miguel Brondani <brondani.miguel@gmail.com>
2025-01-03 02:00:58 -0300

    finalizando codigo e documentacao do cemaden

--- cemaden.py
+++ cemaden.py
@@ -1,6 +1,13 @@
 import csv
+import os
 
 def copiar_header_CEMADEN(spamreader, arquivoFinal)->None:
+    """Copia o header de um arquivo csv em outro
+
+    Args:
+        spamreader (_type_): Dados do csv
+        arquivoFinal (_type_): Arquivo onde o header será escrito
+    """
     for row in spamreader:
         for item in row:
             arquivoFinal.write(item+';')
@@ -8,13 +15,44 @@ def copiar_header_CEMADEN(spamreader, arquivoFinal)->None:
     arquivoFinal.write('\n')
 
 def pega_nome(spamreader)->str:
+    """Retorna o nome do município disponível no arquivo
+
+    Args:
+        spamreader (_type_): Dados do csv
+
+    Returns:
+        str: nome do município dos dados
+    """
     linha2 = False
     for line in spamreader:
         if linha2:
             return line[0]
         linha2= True
 
+def pega_mes_ano(spamreader)->str:
+    """Retorna o mes e ano das medicoes contidas no arquivo do cemaden
+
+    Args:
+        spamreader (_type_): Dados do csv
+
+    Returns:
+        str: ano e número dos dados no formato (YYYY-MM) 
+    """
+    linha2 = False
+    for line in spamreader:
+        if linha2:
+            return line[6][:7]
+        linha2= True
+
 def copiar_meio_final(spamreader, arquivoFinal, dataComeco)->None:
+    """Copia os dados do arquivo a partir da dataComeco
+
+    Args:
+        spamreader (_type_): Dados do csv
+        arquivoFinal (_type_): Arquivo onde será escrito os dados
+        dataFinal (_type_): Data final, condição de inicialização de cópia.
+    """
+
     achouPeriodoInicial = False
     for row in spamreader:
         if achouPeriodoInicial:
@@ -25,45 +63,75 @@ def copiar_meio_final(spamreader, arquivoFinal, dataComeco)->None:
             achouPeriodoInicial = True
 
 def copiar_comeco_meio(spamreader, arquivoFinal, dataFinal)->None:
+    """Copia os dados do arquivo até encontrar a dataFinal
+
+    Args:
+        spamreader (_type_): Dados do csv
+        arquivoFinal (_type_): Arquivo onde será escrito os dados
+        dataFinal (_type_): Data final, condição de parada.
+    """
     for row in spamreader:
         if dataFinal not in row[-2]:
             for item in row:
+                # print(item)
                 arquivoFinal.write(item+';')
             arquivoFinal.write('\n')
+        else:
+            break
 
 def renomear_todos_arquivos(path: str):
-    """Renomeia todos os arquivos para o nome da cidade da medicao (conforme o item municipio)
+    """Renomeia todos os arquivos para o nome da cidade da medicao (conforme o item municipio) e o mês do arquivo
 
     Args:
         path (str): Path para a pasta com os arquivos
     """
-    pass
+    arquivosCsv = [file for file in os.listdir(path) if '.csv' in file]
 
-def main():
-    fileAbril = 'Dados-Brutos/CEMADEN/abril/data.csv'
-    fileMaio = ''
+    for file in arquivosCsv:
+        with open(f'{path}/{file}', 'r', encoding="utf8") as f:
+            spamreader = csv.reader(f, delimiter=';')
+            mes = pega_mes_ano(spamreader)
+            nomeEstacao = pega_nome(spamreader)
+        os.rename(f'{path}/{file}', f'{path}/{nomeEstacao}-{mes}.csv')
 
-    dataComeco = '2024-04-27'
-    dataFinal = '2024-04-15'
+def main(pathBrutos:str, dataComeco:str, dataFinal:str)->None:
+    """Tratamento de dados dos arquivos do CEMADEN. Só funciona para pegar datas entre 04/2024 e 05/2024.
 
-    with open(fileAbril, 'r', encoding="utf8") as f:
-        spamreader = csv.reader(f, delimiter=';')
-        pathFinal = f'Dados-Processados/CEMADEN/{pega_nome(spamreader)}.csv'
-        
-        with open(pathFinal, 'w', encoding="utf8") as fFinal:
-            f.seek(0)
-            copiar_header_CEMADEN(spamreader, fFinal)
-            # copiar_meio_final(spamreader, fFinal, dataComeco)
+    Args:
+        pathBrutos (str): path para o diretório com os arquivos de abril e maio
+        dataComeco (str): data inicial de abril que será copiada
+        dataFinal (str): data final que para a cópia dos dados (não inclusa)
+    """
+    renomear_todos_arquivos(pathBrutos)
+
+    todosArquivos = os.listdir(pathBrutos)
+
+    arquivosAbril = [file for file in todosArquivos if '04' in file]
+    arquivosMaio = [file for file in todosArquivos if '05' in file]
+    arquivosFinal = list()
+
+    for fileAbril in arquivosAbril:
+        with open(f'{pathBrutos}/{fileAbril}', 'r', encoding="utf8") as f:
+            spamreader = csv.reader(f, delimiter=';')
+            arquivosFinal.append(f'Dados-Processados/CEMADEN/{pega_nome(spamreader)}.csv')
+            
+            with open(arquivosFinal[-1], 'w', encoding="utf8") as fFinal:
+                f.seek(0)
+                copiar_header_CEMADEN(spamreader, fFinal)
+                copiar_meio_final(spamreader, fFinal, dataComeco)
+
+    for fileMaio, pathFinal in zip(arquivosMaio, arquivosFinal):
+        with open(f'{pathBrutos}/{fileMaio}', 'r', encoding='utf8') as f:
+            spamreader = csv.reader(f, delimiter=';')
+            next(f)
+
+            with open(pathFinal, 'a', encoding="utf8") as fFinal:
+                copiar_comeco_meio(spamreader, fFinal, dataFinal)
 
 
-    #TROCAR ARQUIVO QUE ESTA SENDO ABERTO
-    with open(fileAbril, 'r', encoding='utf8') as f:
-        spamreader = csv.reader(f, delimiter=';')
-        next(f)
-
-        with open(pathFinal, 'a', encoding="utf8") as fFinal:
-            copiar_comeco_meio(spamreader, fFinal, dataFinal)
-
-
-
-main()
+main('Dados-Brutos/CEMADEN', '2024-04-27', '2024-05-15')
+#PROVAVEIS ERROS: 
+#SE TIVER MAIS DE UM ARQUIVOS COM O MESMO {Nome Estacap (municipio)} + {mes/ano}
+#SE A DATA FINAL NAO EXISTIR NO ARQUIVO DE MAIO
+#----
+#em algum momento deixei de seguir a padronização de váriaveis.
